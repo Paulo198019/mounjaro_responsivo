4de483aaaafa69a0ad7621a4ad28b6a45c60088b
Paulo198019 <pauloalexandremonteiro89@gmail.com>
2026-01-06 23:27:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+// Rolagem suave
+const links = document.querySelectorAll('a[href^="#"]');
+links.forEach(a => a.addEventListener('click', e => {
+  const id = a.getAttribute('href').slice(1);
+  const el = document.getElementById(id);
+  if (el) { e.preventDefault(); el.scrollIntoView({behavior: 'smooth', block: 'start'}); }
+}));
